715f1d7a4cbdce31260d054adfc120a537b47d89
hahnjh <hahnjh0426@naver.com>
2024-09-26 10:38:53 +0900

    modify else

--- src/routes/Banner.js
+++ src/routes/Banner.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import html2canvas from 'html2canvas';
-import { fileUploadSelectedImg, fileUploadAllTag } from '../api/fileUploadToS3';
+import React, { useState, useEffect } from "react";
+import html2canvas from "html2canvas";
+import { fileUploadSelectedImg, fileUploadAllTag } from "../api/fileUploadToS3";
 
 const apikey = process.env.REACT_APP_APIKEY;
 
@@ -8,26 +8,26 @@ export default function Banner() {
   const [width, setWidth] = useState(820);
   const [height, setHeight] = useState(100);
   const [padding, setPadding] = useState(10);
-  const [alignItems, setalignItems] = useState('center');
-  const [title, setTitle] = useState('This is a banner!');
+  const [alignItems, setalignItems] = useState("center");
+  const [title, setTitle] = useState("This is a banner!");
   const [titleFontSize, setTitleFontSize] = useState(16);
-  const [subtitle, setSubtitle] = useState('');
+  const [subtitle, setSubtitle] = useState("");
   const [subtitleFontSize, setSubtitleFontSize] = useState(12);
-  const [backgroundColor, setBackgroundColor] = useState('#ffe157');
-  const [fontFamily, setFontFamily] = useState('Arial');
-  const [titleColor, setTitleColor] = useState('#000000');
-  const [subtitleColor, setSubtitleColor] = useState('#555555');
+  const [backgroundColor, setBackgroundColor] = useState("#ffe157");
+  const [fontFamily, setFontFamily] = useState("Arial");
+  const [titleColor, setTitleColor] = useState("#000000");
+  const [subtitleColor, setSubtitleColor] = useState("#555555");
 
-  const [imageUrl, setImageUrl] = useState(''); //이게 test2의 imageSrc
+  const [imageUrl, setImageUrl] = useState(""); //이게 test2의 imageSrc
   const [imageWidth, setImageWidth] = useState(100);
   const [imageHeight, setImageHeight] = useState(100);
   const [imagePositionX, setImagePositionX] = useState(0);
   const [imagePositionY, setImagePositionY] = useState(0);
 
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState(false); //ai 이미지생성중
   const [imageUrls, setImageUrls] = useState([]); //ai 생성된 이미지들
-  const [selectedImageUrl, setSelectedImageUrl] = useState('');
+  const [selectedImageUrl, setSelectedImageUrl] = useState("");
 
   const [base64Image, setBase64Image] = useState(null);
   // const [imageSrc, setImageSrc] = useState(null);
@@ -38,33 +38,34 @@ export default function Banner() {
     width: `${width}px`,
     height: `${height}px`,
     padding: `${padding}px`,
-    justifyContent: 'center',
+    justifyContent: "center",
     backgroundColor: backgroundColor,
-    display: 'flex',
+    display: "flex",
     alignItems: alignItems,
-    border: '1px solid #ccc',
+    border: "1px solid #ccc",
     fontFamily: fontFamily,
-    flexDirection: 'column',
-    boxSizing: 'border-box',
-    position: 'relative',
+    flexDirection: "column",
+    boxSizing: "border-box",
+    position: "relative"
   };
 
   const titleStyle = {
     fontSize: `${titleFontSize}px`,
     color: titleColor,
+    fontWeight: "bold"
   };
 
   const subtitleStyle = {
     fontSize: `${subtitleFontSize}px`,
-    color: subtitleColor,
+    color: subtitleColor
   };
 
   const imageStyle = {
-    position: 'absolute',
+    position: "absolute",
     top: `${imagePositionY}px`,
     left: `${imagePositionX}px`,
     width: `${imageWidth}px`,
-    height: `${imageHeight}px`,
+    height: `${imageHeight}px`
   };
 
   const makeImageFromAi = async (e) => {
@@ -73,26 +74,26 @@ export default function Banner() {
     setImageUrls([]);
 
     try {
-      const res = await fetch('https://api.openai.com/v1/images/generations', {
-        method: 'POST',
+      const res = await fetch("https://api.openai.com/v1/images/generations", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           // Authorization: apikey
-          Authorization: apikey,
+          Authorization: apikey
         },
         body: JSON.stringify({
           prompt: prompt,
           n: 3,
-          size: '256x256',
-        }),
+          size: "256x256"
+        })
       });
 
       const data = await res.json();
       const urls = data.data.map((imageData) => imageData.url);
       setImageUrls(urls);
     } catch (error) {
-      console.error('Error:', error);
-      setImageUrls(['Error generating images.']);
+      console.error("Error:", error);
+      setImageUrls(["Error generating images."]);
     } finally {
       setLoading(false);
     }
@@ -105,7 +106,7 @@ export default function Banner() {
 
   const saveCustomBanner = async () => {
     try {
-      console.log('저장 실행 시작::', selectedImageUrl);
+      console.log("저장 실행 시작::", selectedImageUrl);
 
       let width = 0;
       let height = 0;
@@ -121,10 +122,10 @@ export default function Banner() {
 
       setIsCaptured(true);
 
-      const element = document.getElementById('customBanner');
+      const element = document.getElementById("customBanner");
 
       if (!element) {
-        console.error('customBanner 요소를 찾을 수 없습니다.');
+        console.error("customBanner 요소를 찾을 수 없습니다.");
         return;
       }
 
@@ -142,22 +143,22 @@ export default function Banner() {
         scale: 1,
         useCORS: true, // CORS 관련 문제 해결
         logging: true,
-        backgroundColor: null, // 투명 배경 설정
+        backgroundColor: null // 투명 배경 설정
       });
 
-      const dataURL = canvas.toDataURL('image/png');
+      const dataURL = canvas.toDataURL("image/png");
 
       // 파일명때문에 현재 시간 시분초
       const now = new Date();
-      const hours = String(now.getHours()).padStart(2, '0');
-      const minutes = String(now.getMinutes()).padStart(2, '0');
-      const seconds = String(now.getSeconds()).padStart(2, '0');
+      const hours = String(now.getHours()).padStart(2, "0");
+      const minutes = String(now.getMinutes()).padStart(2, "0");
+      const seconds = String(now.getSeconds()).padStart(2, "0");
       const fileName = `banner${hours}${minutes}${seconds}.png`;
 
       await fileUploadAllTag(dataURL, fileName);
 
       //서버저장후 다시불러오지않고 바로 data로 로컬에 저장 = 서버에 저장로직 지워도됨
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = dataURL; // dataURL을 href로 설정
       link.download = fileName; // 다운로드할 파일명
       document.body.appendChild(link);
@@ -166,14 +167,36 @@ export default function Banner() {
 
       setIsCaptured(false);
     } catch (error) {
-      console.error('이미지 업로드 중 오류 발생:', error);
+      console.error("이미지 업로드 중 오류 발생:", error);
     }
+  };
+
+  const example1set = async () => {
+    setWidth(460);
+    setHeight(150);
+    setPadding(30);
+    setTitle("KB 100세만족 연금보험 무배당");
+    setSubtitle("(100세보증형)");
+    setTitleFontSize(18);
+    setSubtitleFontSize(14);
+    setTitleColor("#000000");
+    setSubtitleColor("#000000");
+    setalignItems("start");
+    setFontFamily("Arial");
+    setPrompt(
+      "hug grandmon and grandpa each other picture look like illerstrate"
+    );
+
+    setImageWidth(130);
+    setImageHeight(130);
+    setImagePositionX(310);
+    setImagePositionY(18);
   };
 
   useEffect(() => {
     if (base64Image) {
       // base64 데이터를 Blob으로 변환
-      const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
+      const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, "");
       const byteCharacters = atob(base64Data);
       const byteNumbers = new Array(byteCharacters.length);
 
@@ -182,7 +205,7 @@ export default function Banner() {
       }
 
       const byteArray = new Uint8Array(byteNumbers);
-      const blob = new Blob([byteArray], { type: 'image/png' });
+      const blob = new Blob([byteArray], { type: "image/png" });
 
       // Blob을 Object URL로 변환하여 이미지로 표시
       const imageUrlBlob = URL.createObjectURL(blob);
@@ -201,34 +224,37 @@ export default function Banner() {
 
   // console.log("현재이미지url :: ", imageUrl);
   // console.log("현재isCaptured :: ", isCaptured);
+  // console.log("프롬프트 :: ", prompt);
+  console.log("프롬프트 :: ", alignItems);
+  console.log("프롬프트 :: ", fontFamily);
 
   return (
-    <div className='wrap'>
+    <div className="wrap">
       <form>
-        <h2 className='title'>Banner</h2>
-        <div className='container'>
-          <div className='box'>
+        <h2 className="title">Banner</h2>
+        <div className="container">
+          <div className="box">
             <label>Width </label>
             <input
-              type='number'
+              type="number"
               value={width}
               onChange={(e) => setWidth(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Height </label>
             <input
-              type='number'
+              type="number"
               value={height}
               onChange={(e) => setHeight(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Padding </label>
             <input
-              type='number'
+              type="number"
               value={padding}
               onChange={(e) => setPadding(e.target.value)}
             />
@@ -237,76 +263,76 @@ export default function Banner() {
           <div>
             <label>Background Color </label>
             <input
-              type='color'
+              type="color"
               value={backgroundColor}
               onChange={(e) => setBackgroundColor(e.target.value)}
             />
           </div>
         </div>
-        <h2 className='title'>Text</h2>
-        <div className='container'>
-          <div className='box'>
+        <h2 className="title">Text</h2>
+        <div className="container">
+          <div className="box">
             <label>Title</label>
             <input
-              className='w100'
-              type='text'
+              className="w100"
+              type="text"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
           </div>
-          <div className='box'>
-            <label className='sub_title'>SubTitle</label>
+          <div className="box">
+            <label className="sub_title">SubTitle</label>
             <textarea
-              className='w100'
-              type='text'
+              className="w100"
+              type="text"
               value={subtitle}
               onChange={(e) => setSubtitle(e.target.value)}
             />
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Title FontSize</label>
             <input
-              type='number'
+              type="number"
               value={titleFontSize}
               onChange={(e) => setTitleFontSize(e.target.value)}
             />
             <span>( px )</span>
           </div>
 
-          <div className='box'>
+          <div className="box">
             <label>Subtitle FontSize</label>
             <input
-              type='number'
+              type="number"
               value={subtitleFontSize}
               onChange={(e) => setSubtitleFontSize(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Title Color</label>
             <input
-              type='color'
+              type="color"
               value={titleColor}
               onChange={(e) => setTitleColor(e.target.value)}
             />
           </div>
-          <div className='box'>
+          <div className="box">
             <label>SubTitle Color</label>
             <input
-              type='color'
+              type="color"
               value={subtitleColor}
               onChange={(e) => setSubtitleColor(e.target.value)}
             />
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Text Align</label>
             <select
               value={alignItems}
               onChange={(e) => setalignItems(e.target.value)}
             >
-              <option value='start'>Left</option>
-              <option value='center'>Center</option>
-              <option value='flex-end'>Right</option>
+              <option value="start">Left</option>
+              <option value="center">Center</option>
+              <option value="flex-end">Right</option>
             </select>
           </div>
           <div>
@@ -315,90 +341,97 @@ export default function Banner() {
               value={fontFamily}
               onChange={(e) => setFontFamily(e.target.value)}
             >
-              <option value='Arial'>Arial</option>
-              <option value='Courier New'>Courier New</option>
-              <option value='Georgia'>Georgia</option>
-              <option value='Times New Roman'>Times New Roman</option>
-              <option value='Verdana'>Verdana</option>
+              <option value="Arial">Arial</option>
+              <option value="Courier New">Courier New</option>
+              <option value="Georgia">Georgia</option>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Verdana">Verdana</option>
             </select>
           </div>
         </div>
-        <h2 className='title'>Image</h2>
-        <div className='container'>
-          <div className='box'>
+        <h2 className="title">Image</h2>
+        <div className="container">
+          <div className="box">
             <label>Image Width</label>
             <input
-              type='number'
+              type="number"
               value={imageWidth}
               onChange={(e) => setImageWidth(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Image Height</label>
             <input
-              type='number'
+              type="number"
               value={imageHeight}
               onChange={(e) => setImageHeight(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Image Position X</label>
             <input
-              type='number'
+              type="number"
               value={imagePositionX}
               onChange={(e) => setImagePositionX(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box'>
+          <div className="box">
             <label>Image Position Y</label>
             <input
-              type='number'
+              type="number"
               value={imagePositionY}
               onChange={(e) => setImagePositionY(e.target.value)}
             />
             <span>( px )</span>
           </div>
-          <div className='box' style={{ borderBottom: 'none' }}>
+          <div className="box" style={{ borderBottom: "none" }}>
             <label>Image URL</label>
             <input
-              className='w100'
-              type='text'
+              className="w100"
+              type="text"
               value={imageUrl}
               onChange={(e) => setImageUrl(e.target.value)}
             />
           </div>
         </div>
       </form>
-      <div className='form_box'>
+      <div className="form_box">
         <label>Generated Images</label>
         <form onSubmit={makeImageFromAi}>
           <textarea
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
-            placeholder='Enter prompt for image generation'
+            placeholder="Enter prompt for image generation"
           />
-          <button type='submit' disabled={loading}>
-            {loading ? 'Generating...' : 'Confirm'}
+          <button type="submit" disabled={loading}>
+            {loading ? "Generating..." : "Confirm"}
+          </button>
+          <button
+            type="button"
+            disabled={loading}
+            onClick={() => setImageUrl("")}
+          >
+            삭제
           </button>
         </form>
       </div>
       {imageUrls.length > 0 && (
         <div>
-          <h2 className='title'>Generated Images:</h2>
-          <div style={{ display: 'flex', gap: '10px', margin: '10px 0' }}>
+          <h2 className="title">Generated Images:</h2>
+          <div style={{ display: "flex", gap: "10px", margin: "10px 0" }}>
             {imageUrls.map((url, index) => (
               <div key={index}>
                 <img
                   src={url}
                   alt={`Generated ${index + 1}`}
                   style={{
-                    maxWidth: '150px',
-                    maxHeight: '150px',
-                    cursor: 'pointer',
-                    border: selectedImageUrl === url ? '3px solid red' : 'none',
+                    maxWidth: "150px",
+                    maxHeight: "150px",
+                    cursor: "pointer",
+                    border: selectedImageUrl === url ? "3px solid red" : "none"
                   }}
                   onClick={() => handleImageSelection(url)}
                 />
@@ -408,23 +441,26 @@ export default function Banner() {
         </div>
       )}
       {/* Banner output */}
-      <div id='customBanner' style={bannerStyle}>
+      <div id="customBanner" style={bannerStyle}>
         <div style={titleStyle}>{title}</div>
         {subtitle && (
-          <div style={{ ...subtitleStyle, whiteSpace: 'pre-wrap' }}>
+          <div style={{ ...subtitleStyle, whiteSpace: "pre-wrap" }}>
             {subtitle}
           </div>
         )}
         {imageUrl && (
           <img
             src={isCaptured ? imageUrl : selectedImageUrl}
-            alt='wrong img addr'
+            alt="wrong img addr"
             style={imageStyle}
           />
         )}
       </div>
-      <button className='btn' onClick={saveCustomBanner}>
+      <button className="btn" onClick={saveCustomBanner}>
         Save
+      </button>
+      <button className="btn" onClick={example1set}>
+        sample 1
       </button>
     </div>
   );
